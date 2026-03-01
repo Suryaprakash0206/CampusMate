69639512a5d8230838d6be88c5da167799cf246f
Vijaya723805 <vijayagaraga84@gmail.com>
2026-03-01 19:24:18 +0530

    Update faculty.js

--- backend/models/faculty.js
+++ backend/models/faculty.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const facultySchema = new mongoose.Schema({
   facultyId: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  name: { type: String }
+  fullName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phoneNumber: { type: String, required: true },
+  department: { type: String, required: true },
+  password: { type: String, required: true }
 });
 
 export default mongoose.model("Faculty", facultySchema);
